232c8ba40c99eaade41368469bbc30a909c06c69
Alex Pojman <pojman.alex@gmail.com>
2014-07-29 21:10:24 -0500

    Initial Commit

---
+++ LastPiece/YBIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YBIAppDelegate.h
+//  LastPiece
+//
+//  Created by Alex Pojman on 7/29/14.
+//  Copyright (c) 2014 Ya Boi Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YBIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LastPiece/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LastPiece
+//
+//  Created by Alex Pojman on 7/29/14.
+//  Copyright (c) 2014 Ya Boi Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YBIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YBIAppDelegate class]));
+    }
+}
